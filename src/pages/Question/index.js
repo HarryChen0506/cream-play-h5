9ccec0990b68c043bbc26b2ffb665ef2694c0550
HarryChen0506 <chenwenhua0506@sina.com>
2021-01-05 13:30:57 +0800

    feat: question ui

--- src/pages/Question/index.js
+++ src/pages/Question/index.js
@@ -4,10 +4,22 @@ import React, { useState, useCallback } from 'react';
 // import { useHistory } from 'react-router-dom';
 import './index.less'
 
+
 const Question = () => (
   <div className="page-question">
     <div className="page-body">
-      12
+      <div className='question-section'>
+        <div className='title'>请问小朋友</div>
+        <div className='question-body'>
+          <div className='option-item'>
+            <div className='result'></div>
+          </div>
+          <div className='option-item'>
+            <img src='/assets/question/topic_1/勺.png'></img>
+          </div>
+          <div className='option-item'></div>
+        </div>
+      </div>
     </div>
   </div>
 );
