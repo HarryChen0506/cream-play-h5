c071cf8762b17d157a5dcfc53f7313a4c7b1a519
HarryChen0506 <chenwenhua0506@sina.com>
2021-01-08 20:19:28 +0800

    feat: drag

--- src/pages/Question/Drag/index.js
+++ src/pages/Question/Drag/index.js
@@ -7,9 +7,9 @@ import './index.less';
 // eslint-disable-next-line no-undef
 const PUBLIC_URL = __PUBLIC_URL__;
 
-const Dragger = ({
+const Dragger = forwardRef(({
   left, top, children, zIndex, draggable = true, onMount, onEnd, visiable,
-}) => {
+}, ref) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const defaultPosition = useRef();
   const positionRef = useRef();
@@ -20,16 +20,12 @@ const Dragger = ({
 
   useLayoutEffect(() => {
     const dom = containerRef.current;
-    // console.log('containerRef', dom);
     if (dom) {
       const parentDom = document.querySelector('#dragger-container');
       const parentRect = parentDom.getBoundingClientRect();
       const domRect = dom.getBoundingClientRect();
       const offsetX = domRect.x - parentRect.x;
       const offsetY = domRect.y - parentRect.y;
-      // console.log('parrent');
-      // console.log('containerRef');
-      // console.log(offsetX, offsetY);
       defaultPosition.current = { x: offsetX, y: offsetY };
       setPosition({
         x: offsetX,
@@ -86,6 +82,15 @@ const Dragger = ({
     document.addEventListener('touchend', onTouchend);
   }, [draggable, onTouchend, onTouchmove]);
 
+  useImperativeHandle(ref, () => ({
+    reset: () => {
+      setPosition({
+        x: defaultPosition.current.x,
+        y: defaultPosition.current.y,
+      });
+    },
+  }));
+
   return (
     <div
       className={clsx({
@@ -103,7 +108,7 @@ const Dragger = ({
       {children}
     </div>
   );
-};
+});
 
 const calcDistance = (point1, point2) => {
   const sum = (point1.x - point2.x) * (point1.x - point2.x)
@@ -111,40 +116,55 @@ const calcDistance = (point1, point2) => {
   return Math.sqrt(sum, 0.5);
 };
 
-const Group = () => {
+const Group = ({ onSuccess, origin = {}, target = {} }) => {
   const [success, setSuccess] = useState(false);
   const shapeTarget = useRef();
   const shapeOrigin = useRef();
+  const shapeOriginRef = useRef();
   const shapeTargetMount = (data) => {
     shapeTarget.current = data;
-    console.log('shape target', shapeTarget.current);
+    // console.log('shape target', shapeTarget.current);
   };
   const shapeOriginEnd = (data) => {
     shapeOrigin.current = data;
-    console.log('shape origin', shapeOrigin.current);
+    // console.log('shape origin', shapeOrigin.current);
     const distance = calcDistance(shapeOrigin.current, shapeTarget.current);
-    if (distance < 10) {
+    // console.log('distance', distance);
+    if (distance < 15) {
       setSuccess(true);
+      typeof onSuccess === 'function' && onSuccess();
+    } else {
+      const originComp = shapeOriginRef.current;
+      originComp.reset();
     }
-    console.log('distance', distance);
   };
   return (
     <>
-      <Dragger visiable={success} left="20%" top="38%" zIndex={2} draggable={false} onMount={data => shapeTargetMount(data)}>
-        <div className="yuanzhu-size" style={{ opacity: 0.3 }}>
-          <img className="img" src={`${PUBLIC_URL}/assets/question/B11/yuanzhu.png`} />
-        </div>
+      <Dragger
+        visiable={success}
+        left={target.left}
+        top={target.top}
+        zIndex={target.zIndex}
+        draggable={false}
+        onMount={data => shapeTargetMount(data)}
+      >
+        {target.node}
       </Dragger>
-      <Dragger visiable={!success} left="50%" top="50%" zIndex={2} onEnd={data => shapeOriginEnd(data)}>
-        <div className="yuanzhu-size">
-          <img className="img" src={`${PUBLIC_URL}/assets/question/B11/yuanzhu.png`} />
-        </div>
+      <Dragger
+        visiable={!success}
+        left={origin.left}
+        top={origin.top}
+        zIndex={origin.zIndex}
+        onEnd={data => shapeOriginEnd(data)}
+        ref={shapeOriginRef}
+      >
+        {origin.node}
       </Dragger>
     </>
   );
 };
 
-const Drag = (props) => {
+const DragPlay = (props) => {
   const { info = {} } = props;
   // const { } = info;
 
@@ -155,18 +175,53 @@ const Drag = (props) => {
       </div>
       <div className="xuxian-wrap xuxian-size">
         <img className="img" src={`${PUBLIC_URL}/assets/question/B11/xuxian.png`} />
-
-        {/* <div className="shu-inner-pos shu-size">
-          <img className="img" src={`${PUBLIC_URL}/assets/question/B11/shu.png`} />
-        </div> */}
       </div>
-      <Group />
-
-      <div className="shu-outer-pos shu-size" draggable>
-        <img className="img" src={`${PUBLIC_URL}/assets/question/B11/shu.png`} />
-      </div>
+      <Group
+        target={{
+          left: '20%',
+          top: '39%',
+          zIndex: 2,
+          node: (
+            <div className="yuanzhu-size" style={{ opacity: 0.8 }}>
+              <img className="img" src={`${PUBLIC_URL}/assets/question/B11/yuanzhu.png`} />
+            </div>
+          ),
+        }}
+        origin={{
+          left: '50%',
+          top: '50%',
+          zIndex: 2,
+          node: (
+            <div className="yuanzhu-size">
+              <img className="img" src={`${PUBLIC_URL}/assets/question/B11/yuanzhu.png`} />
+            </div>
+          ),
+        }}
+      />
+      <Group
+        target={{
+          left: '20%',
+          top: '65%',
+          zIndex: 1,
+          node: (
+            <div className="shu-size">
+              <img className="img" src={`${PUBLIC_URL}/assets/question/B11/shu.png`} />
+            </div>
+          ),
+        }}
+        origin={{
+          left: '80%',
+          top: '50%',
+          zIndex: 1,
+          node: (
+            <div className="shu-size" draggable>
+              <img className="img" src={`${PUBLIC_URL}/assets/question/B11/shu.png`} />
+            </div>
+          ),
+        }}
+      />
     </div>
   );
 };
 
-export default Drag;
+export default DragPlay;
