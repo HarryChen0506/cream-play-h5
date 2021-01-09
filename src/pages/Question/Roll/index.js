b7c2a350e20263c986640eb3fe334246ec0bd12f
HarryChen0506 <chenwenhua0506@sina.com>
2021-01-09 22:11:58 +0800

    fix: rolling

--- src/pages/Question/Roll/index.js
+++ src/pages/Question/Roll/index.js
@@ -5,35 +5,58 @@ import clsx from 'clsx';
 import './index.less';
 
 const PUBLIC_URL = __PUBLIC_URL__;
+
+const RollObject = ({ onSuccess }) => {
+  const [rolling, setRolling] = useState(false);
+  const start = useRef();
+  const onTouchStart = useCallback((e) => {
+    const touch = e.targetTouches[0];
+    start.current = { pageX: touch.pageX };
+  }, []);
+  const onTouchEnd = useCallback((e) => {
+    const touch = e.changedTouches[0];
+    if (touch.pageX - start.current.pageX > 20) {
+      setRolling(true);
+      setTimeout(() => {
+        typeof onSuccess === 'function' && onSuccess();
+      }, 3000);
+    }
+  }, [onSuccess]);
+  return (
+    <div className="rolling-wrap">
+      <img
+        className={clsx({
+          image: true,
+          dao: true,
+          roll: rolling,
+        })}
+        src={`${PUBLIC_URL}/assets/question/M7/dao.png`}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
+      />
+      {!rolling && <img className="tip" src={`${PUBLIC_URL}/assets/question/M7/tip.png`} />}
+    </div>
+  );
+};
+
 const RollPlay = (props) => {
   const { info, onSuccess } = props;
-  const [rolling, setRolling] = useState(false);
+  const [fall, setFall] = useState(false);
   const handleClick = useCallback(() => {
-    setRolling(true);
-    setTimeout(() => {
-      typeof onSuccess === 'function' && onSuccess();
-    }, 3000);
-  }, [onSuccess]);
+    setFall(true);
+  }, []);
   return (
     <div className="component-roll">
       <div className="image-wrap">
-        <img
-          className={clsx({
-            img: true,
-            hidden: rolling,
-          })}
-          src={`${PUBLIC_URL}/assets/question/M7/zhan.png`}
-          onClick={handleClick}
-        />
-        <img
-          className={clsx({
-            img: true,
-            dao: true,
-            roll: rolling,
-            hidden: !rolling,
-          })}
-          src={`${PUBLIC_URL}/assets/question/M7/dao.png`}
-        />
+        {!fall ? (
+          <img
+            className={clsx({
+              'image-stand': true,
+            })}
+            src={`${PUBLIC_URL}/assets/question/M7/zhan.png`}
+            onClick={handleClick}
+          />
+        ) : <RollObject onSuccess={onSuccess} />}
       </div>
     </div>
   );
